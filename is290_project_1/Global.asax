d07fafa4a784ce961c898c1ce93a81e06e2e947a
Keegan <Keegan@KEEGAN-PC>
2018-10-10 00:20:10 -0500

    Add project files.

---
+++ is290_project_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="is290_project_1.MvcApplication" Language="C#" %>
---
+++ is290_project_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(is290_project_1.Startup))]
+namespace is290_project_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
