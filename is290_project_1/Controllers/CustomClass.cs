fb8fd162b6c998fb21f478d9ef4d33237e1f5a6e
Rileigh Bickel <bickel43@ymail.com>
2018-11-06 11:15:00 -0600

    minor changes

---
+++ is290_project_1/Controllers/CustomClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using is290_project_1.Models;
+
+namespace is290_project_1.Custom
+{
+    public class CustomClass
+    {
+        public void GetNumberOfEmps()
+        {
+            Employee employeeModel = new Employee();
+
+            int count = 0;
+
+            foreach (var employee in Convert.ToString(employeeModel))
+            {
+                count++;
+            }
+
+            Console.Write(count.ToString());
+        }
+    }
+}
