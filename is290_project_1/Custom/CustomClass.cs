b6fbb68c2462adc177d40657cd40aac03cf0d4ff
Keegan Bickel <bickel43@ymail.com>
2018-11-06 02:01:51 -0600

    vbnfgh

---
+++ is290_project_1/Custom/CustomClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using is290_project_1.Models;
+
+namespace is290_project_1.Custom
+{
+    public class CustomClass
+    {
+        //initialize db context and use items from it
+
+        public void GetEmployeeID()
+        {
+            is290dbEntities1 dbModel = new is290dbEntities1();
+            int count = dbModel.Employees.Count();
+
+        }
+        public void GetDepartmentID()
+        {
+
+        }
+    }
+}
